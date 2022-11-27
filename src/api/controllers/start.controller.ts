6e12de57c52686271cb5d6f21b177517c2f471ca
Marko Perović <mmarko.perovici3@gmail.com>
2022-11-27 22:31:28 +0100

    added services and db queries

--- src/api/controllers/start.controller.ts
+++ src/api/controllers/start.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { APIError } from "../errors/api-error";
+import { StartService } from "../services/start.service";
 
 export const index = (req: Request, res: Response, next: NextFunction): void => {
   try {
     res.status(200).json({
-      message: "Server is live"
+      message: StartService.getStartMessage()
     });
   } catch (error) {
     throw new APIError(error?.message, error?.status);
